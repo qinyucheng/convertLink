3442cbeb13e0fd87d30c3ae04c6db7fb09963a6e
qinyucheng <qinyucheng711@gmail.com>
2016-05-27 10:12:04 -0500

    0527

---
+++ Controllers/linkConvertController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using convertLink.Models;
+
+namespace convertLink.Controllers
+{
+    public class linkConvertController : ApiController
+    {
+        public bool post(converter lessonData)
+        {
+
+            bool result=lessonData.creatNameListJsFile();
+
+            return result;
+
+        }
+    }
+}
---
+++ Models/converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.IO;
+
+namespace convertLink.Models
+{
+    public class converter
+    {
+         public string linkJS { get; set; }
+         public string projectname { get; set; }
+
+
+        public bool creatNameListJsFile() {
+            string storePath = ConfigurationManager.AppSettings["folderPath"];
+            string storeData="";
+
+            storeData = linkJS;
+            try {
+                StreamWriter sr = File.CreateText(storePath +"\\link_convert.js"); 
+                sr.Write(storeData);
+                sr.Flush();
+                sr.Close();
+                return true;
+            }
+            catch(Exception e){
+                return false;
+
+            }
+         }
+
+
+    }
+}
